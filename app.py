2c2f66af3ceb8a0f17dd8f47cb5a49e6c117acc7
MrManz <vincentmanz@aol.com>
2024-09-05 23:18:00 +0200

    Changed logging

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hallo aus Container"
+    return "Hallo aus dem Container"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
